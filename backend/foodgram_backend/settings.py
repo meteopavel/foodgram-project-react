0675ebbea48d83ffa5e6c03ff034bb5a4300f64d
meteopavel <isu@mateopavel.ru>
2023-12-08 08:57:27 +0800

    containers

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', ' ')
-#
+
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(' ')
